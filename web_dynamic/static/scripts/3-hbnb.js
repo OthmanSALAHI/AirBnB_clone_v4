705885e85bddf44f2d360d0a5c9d0297780de45f
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-10 12:52:33 +0100

    Fix formatting in 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -31,7 +31,7 @@ $(document).ready(() => {
       for (let i = 0; i < data.length; i++) {
         const place = data[i];
         html_inner = '<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>';
-        $('.places ').append(html_inner);
+        $('.places').append(html_inner);
       }
     }
   });
